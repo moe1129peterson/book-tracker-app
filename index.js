887c16940df6e5a49dd9d017de52d51a6e7f1c95
moe1129peterson <moe1129tsuzuki@gmail.com>
2021-11-12 11:01:43 -0700

    add start

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express'); 
 const app = express(); 
-app.get('/', (req, res) => {
+app.get('/', function (req, res) => {
      res.send ('It is working!')
 })
 app.listen(process.env.PORT || 5000) 
