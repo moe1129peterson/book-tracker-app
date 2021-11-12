166212d23cc10918ca27cff40761a89c4b4cfb7e
moe1129peterson <moe1129tsuzuki@gmail.com>
2021-11-12 11:15:10 -0700

    fixed again

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express'); 
 const app = express(); 
-app.get('/', function (req, res) => {
-     res.send ('It is working!'); 
+app.get('/', function (req, res) {
+    res.send ('It is working!'); 
 }); 
 
 app.listen(process.env.PORT || 5000); 
